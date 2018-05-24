f1ed783eaca0b231ac65108a7e5c78b3e63934c1
Stephanie Oliveira <stearruda@gmail.com>
2018-05-24 23:54:30 +0200

    Add function to check Enemy-Player collision

--- js/engine.js
+++ js/engine.js
@@ -79,7 +79,7 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        checkCollisions();
     }
 
     /* This is called by the update function and loops through all of the
@@ -94,6 +94,12 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
         player.update();
+    }
+
+    function checkCollisions() {
+        allEnemies.forEach(function(enemy) {
+            enemy.checkCollisions();
+        });
     }
 
     /* This function initially draws the "game level", it will then call
